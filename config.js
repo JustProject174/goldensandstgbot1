c1570cf0e932671493580c116e78867041c1bc0b
Herreh174 <117072709+Herreh174@users.noreply.github.com>
2025-07-27 15:02:51 +0500

    Добавлена БД вместо базы знаний из txt

--- config.js
+++ config.js
@@ -4,8 +4,6 @@ module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN,
 	SUPABASE_URL: process.env.SUPABASE_URL, // Например, "https://your-project.supabase.co"
     SUPABASE_KEY: process.env.SUPABASE_KEY, // Ваш публичный или приватный ключ
-    #KNOWLEDGE_BASE_FILE: "knowledge_base.txt",
-    #ADMIN_ANSWERS_FILE: "admin_answers.txt",
     ADMIN_CHAT_ID: "-1002826990012",
     // Данные номеров теперь загружаются из локального файла rooms/rooms.csv
 };
